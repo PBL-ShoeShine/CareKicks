4e78ab71a41d93daf9a728f7557abdffd4a726c0
Zaxzz <hasyahhamm@gmail.com>
2026-06-07 13:33:32 +0700

    add qr

--- lib/core/network/api_service.dart
+++ lib/core/network/api_service.dart
@@ -6,7 +6,7 @@ import 'package:flutter/foundation.dart';
 import '../auth/session_manager.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://172.16.177.32:3000/api/v1';
+  static const String baseUrl = 'http://172.16.160.147:3000/api/v1';
 
   // ─── Role Helpers ─────────────────────────────────────────────────────────
 
